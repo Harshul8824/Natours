a2a0b55bf5e4e70d1b3711bb4258b7e575ccfea1
Harshul Goswami <goswamiharshul88@gmail.com>
2026-02-04 17:36:46 +0530

    crete the fresh frontend using vite in the react-frontend branch and write code of app.jsx for chekcthe api data is fetch successfully

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/tours")
+      .then(res => res.json())
+      .then(data => {
+        console.log("API DATA 👉", data); // browser console
+        setTours(data.data.data);         // ✅ set state
+      })
+      .catch(err => console.error(err));
+  }, []);
+
+  console.log(tours);
+
+  return (
+    <div>
+      <h1>Natours Tours</h1>
+      {tours.map(tour => (
+        <div key={tour._id}>
+          <h3>{tour.name}</h3>
+          <p>₹{tour.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
